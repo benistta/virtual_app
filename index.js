9e76e0908c698191f29bc81320bdf94098d75e1a
Becky Nisttahuz <benistta@hotmail.com>
2023-06-24 14:12:23 -0600

    base setup express server

---
+++ index.js
@@ -0,0 +1,10 @@
+// A express server, which will handle api request which will handle api requests coming in and responde back with a json object, it will use body parser as well as cors.
+
+const express = require('expresss');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+app.use(cors());

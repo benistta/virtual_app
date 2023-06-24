fc5d37ab90314a9556b5e8232b6e5a2521f961cb
Becky Nisttahuz <benistta@hotmail.com>
2023-06-24 14:57:04 -0600

    test routes

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // A express server, which will handle api request which will handle api requests coming in and responde back with a json object, it will use body parser as well as cors.
 
-const express = require('expresss');
+const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
@@ -9,6 +9,12 @@ const port = 3000;
 app.use(bodyParser.json());
 app.use(cors());
 
+// set up the routes
+
 app.get('/', (req, res) => {
-    res.send('Hello World');
-});    
+    res.send('Hello World')
+})
+
+app.listen(port, () => {
+    console.log('example app listening') 
+});

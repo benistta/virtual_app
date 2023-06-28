b7ccea472153248aa2c9d3b113e6372a854d20a9
Becky Nisttahuz <benistta@hotmail.com>
2023-06-27 20:28:33 -0600

    Add openai Node.js package to index.js

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 // A express server, which will handle api request which will handle api requests coming in and responde back with a json object, it will use body parser as well as cors.
 
+//copy openai Node.js package:
+
+import { Configuration, OpenAIApi } from "openai";
+const configuration = new Configuration({
+    organization: "org-kx8e4puhUptTJ4M8NfLNcf9s",
+    apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+const response = await openai.listEngines();
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
